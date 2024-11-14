0ee77a986de9f2e0625d0a161ee7ce6dc4fd0d82
F1PWE <149507438+F1PWE@users.noreply.github.com>
2024-11-14 02:15:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
 echo "cloning git repo"
 git clone https://github.com/F1PWE/.suckless.git $HOME/.my_dwm
 sleep .5
+echo "install packages"
+
+if [ -f $HOME/.my_dwm_setup/requirements.txt ]; then
+    echo "Installing required packages..."
+    sudo pacman -S --needed $(cat $HOME/.my_dwm_setup/requirements.txt | xargs)
+else
+    echo "requirements.txt not found. Skipping package installation."
+fi
+
 echo "extracting dot files to home."
 cp -r $HOME/.my_dwm/dotfiles/* $HOME/
 sleep .5
